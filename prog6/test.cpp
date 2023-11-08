930ee583d9edbe23555334d29974d055d878ff2f
cjmzone <cjm.zone@gmail.com>
2023-11-08 15:24:48 -0800

    compute() is computing

---
+++ prog6/test.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+int add(int a, int b) {
+    cout << "Inside add function=> a:"<<a<<" b:"<<b<<endl;
+    int c = 0;
+    c = a + b;
+    //int x = c - a;
+    if (c > 0)
+        cout << "no over flow\n";
+    else
+        throw 3.14;
+    return c;
+}
+
+int main() {
+    cout << "MAX INt:"<< INT_MAX << endl;
+    try {
+        cout << add(INT_MAX, 5) << endl;
+        cout << "happy happy\n";
+    }
+    catch (...) {
+        cout << "error caught:"<<endl;
+    }
+    cout << "lucky day!\n";
+    return 0;
+}
--- prog6/threeN.cpp
+++ prog6/threeN.cpp
@@ -1,8 +1,63 @@
 #include <iostream>
+#include <limits.h>
+#include <vector>
+
+using std::vector;
 using namespace std;
 
-int main() {
-	
-	return 0;
+int compute(int num, int &max, int &evenCount, int &oddCount, int &steps){
 
+	// if num is even
+	if(num % 2 != 1) {
+		evenCount++;
+		steps++;
+		int next = num / 2;
+		if (next > max ) {
+			max = next;
+		}
+		if(next == 1) {
+			return 0;
+		}
+		cout << "->(" << next << ")";
+		return compute(next, max, evenCount, oddCount, steps);
+	} else { // num is odd 
+		oddCount++;
+		steps++;
+		int next = 3*(num) + 1;
+		if (next > max ) {
+			max = next;
+		}
+		if(next == 1) {
+			return 0;
+		}
+		cout << "->(" << next << ")";
+		return compute(next, max, evenCount, oddCount, steps);
+	}
+
+}
+
+int main(int argc, char* argv[]) {
+	int evenCount = 0;
+	int oddCount = 0;
+	int steps = 0;
+	int start = 0;
+	if (argc == 1 ) {
+		cout << "Enter a 3n+1 candidate: "; 
+		cin >> start;
+		int max = start;
+		cout << "->(" << start << ")";
+		compute(start, max, evenCount, oddCount, steps);
+		cout << endl;
+		cout << "max: " << max << endl; 
+		cout << "evens: " << evenCount << endl; 
+		cout << "odds: " << oddCount << endl; 
+		cout << "steps: " << steps << endl; 
+
+	} else {
+		for (int i = 1; i < argc; i++) {
+			start = stoi(argv[i]);
+		}
+	}
+
+	return 0;
 }
