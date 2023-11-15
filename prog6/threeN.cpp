0b2a33f024d35f793e476f87fa2589a53cde0ad6
cjmzone <cjm.zone@gmail.com>
2023-11-15 11:46:40 -0800

    Program 6 complete

--- prog6/threeN.cpp
+++ prog6/threeN.cpp
@@ -1,55 +1,73 @@
+// Connor McKenna
+// (CECS 325-01)
+// Program 6 - 32 + 1
+// Due Date (11/15/2023)
+// I certify that this program is my own original work. I did not copy any part of this program from
+// any other source. I further certify that I typed each and every line of code in this program.
+
 #include <iostream>
 #include <limits.h>
 
-using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::stoi;
 
 int compute(int num, int &max, int &evenCount, int &oddCount, int &steps) {
 	// check for integer overflow
+	int next = 0;
 	if(num >! INT_MAX) {
+
 		// if num is even
 		if (num % 2 == 0) {
 			evenCount++;
 			steps++;
-			int next = num / 2;
+
+			next = num / 2;
 			if (next > max) {
 				max = next;
 			}
 
-			if (next == 1) {
+			if (next < 0 ) {
+				throw num;
+				
+			} else if (next == 1) {
 				steps++;
 				cout << "->(" << next << ")";
 				return 0;
+			} else {
+				cout << "->(" << next << ")";
+				return compute(next, max, evenCount, oddCount, steps);
 			}
-
-			cout << "->(" << next << ")";
-			return compute(next, max, evenCount, oddCount, steps);
-
 		// if num is odd
   		} else {
 
 			oddCount++;
 			steps++;
-			int next = 3 * num + 1;
+			next = 3 * num + 1;
 			if (next > max) {
 				max = next;
 			}
 
-			if (next == 1) {
+			if (next < 0 ) {
+				throw num;
+
+			} else if (next == 1) {
 				steps++;
 				cout << "->(" << next << ")";
 				return 0;
+			} else {
+				cout << "->(" << next << ")";
+				return compute(next, max, evenCount, oddCount, steps);
 			}
-
-			cout << "->(" << next << ")";
-			return compute(next, max, evenCount, oddCount, steps);
   		}
 	} else {
-		throw 3.14;
+		throw num;
 	}
 }
 
 int main(int argc, char* argv[]) {
-	cout << "Max int: " << INT_MAX << endl; 
+
 	if (argc == 1) {
 		int evenCount = 0;
 		int oddCount = 0;
@@ -58,44 +76,54 @@ int main(int argc, char* argv[]) {
 
 		cout << "Enter a 3n+1 candidate: ";
 		cin >> start;
-
 		int max = start;
+
 		cout << "->(" << start << ")";
 		try {
 	    		compute(start, max, evenCount, oddCount, steps);
-			} catch (...) {
-				cout << "ERROR caught:" << endl;
+			} catch (int overflow) {
+				cout << "->(###overflow###)" << endl << endl;
+				cout << "overflow detected for: " << overflow << endl;
+				cout << "3n + 1: " << 3 * (overflow + 1) << endl; 
+				cout << "something broke, dude :(" << endl;
 			}
-		cout << endl;
 
+		cout << endl;
 		cout << "start: " << start << endl;
-		cout << "steps: " << steps << endl;
+		cout << "steps: " << steps - 1 << endl;
 		cout << "max: " << max << endl;
 		cout << "odds: " << oddCount << endl;
-		cout << "evens: " << evenCount << endl;
+		cout << "evens: " << evenCount << endl << endl;
+
  	} else {
+
     	for (int i = 1; i < argc; i++) {
       		int evenCount = 0;
       		int oddCount = 0;
       		int steps = 0;
      		int start = stoi(argv[i]);
-
      		int max = start;
+
+			cout << "Solving 3n + 1 - starting value: " << start << endl;
      		cout << "->(" << start << ")";
 
 			try {
 	    		compute(start, max, evenCount, oddCount, steps);
-			} catch (...) {
-				cout << "->(###overflow###)" << endl;
+			} catch (int overflow) {
+				cout << "->(###overflow###)" << endl << endl;
+				cout << "overflow detected for: " << overflow << endl;
+				cout << "3n + 1: " << 3 * (overflow + 1) << endl; 
+				cout << "something broke, dude :(" << endl << endl;
+				continue;
 			}
-      		cout << endl;
 
+      		cout << endl;
      		cout << "start: " << start << endl;
-      		cout << "steps: " << steps << endl;
+      		cout << "steps: " << steps - 1 << endl;
       		cout << "max: " << max << endl;
       		cout << "odds: " << oddCount << endl;
-      		cout << "evens: " << evenCount << endl;
+      		cout << "evens: " << evenCount << endl << endl;
 		}
 	}
-  return 0;
+  	return 0;
 }
